dba394ecf84fb943ae1cfb57372de8598f7a5329
Daniel Runyan <drunyan28.3@gmail.com>
2019-11-25 13:31:52 -0700

    started working on clean up script

--- TrackingStuff/CleanKeyFrameJson.py
+++ TrackingStuff/CleanKeyFrameJson.py
@@ -14,15 +14,57 @@ def main(argv):
             print("File path {} does not exist.".format(file_name))
             continue
 
-        # with open(file_name) as file:
-            # lines = file.readlines()
+        with open(file_name) as file:
+            data = json.load(file)
 
-    print("don't forget to write the clean up script")
+        for keys in data:
+            print(keys)
+            name = keys['Name']
+            if 'Scale' in name:
+                cleanScaleKeys(keys)
+            elif 'Position' in name:
+                cleanPositionKeys(keys)
+            elif 'Rotation' in name:
+                cleanRotationKeys(keys)
+
+        dumpJsonToFile(file_name, data)
 
 
-def dumpJsonToFile(file_name, scale_x_keys, scale_y_keys, position_x_keys, position_y_keys, rotation_keys):
-    data = [scale_x_keys, scale_y_keys, position_x_keys, position_y_keys, rotation_keys]
+# TODO set tols
+def cleanScaleKeys(keys):
+    tol = 0
+    cleanKeys(keys, tol)
 
+
+def cleanPositionKeys(keys):
+    tol = 0
+    cleanKeys(keys, tol)
+
+
+def cleanRotationKeys(keys):
+    tol = 0
+    cleanKeys(keys, tol)
+
+
+def cleanKeys(keys, tol):
+    itr = iter(keys)
+    keys_to_remove = []
+    while True:
+        try:
+            element = next(itr)
+            # todo write function
+            # check if next element is in tol
+            # if yes, search for more and add all middle keys to remove list
+
+        except StopIteration:
+            break
+
+    for key in keys_to_remove:
+        if key in keys:
+            del keys[key]
+
+
+def dumpJsonToFile(file_name, data):
     with open(file_name, 'w') as file:
         json.dump(data, file, indent=2)
 
